f26dd1760636761b72413e2f8a107539e497a212
ThiagoCav <thiagocavallari@outlook.com>
2020-03-02 23:58:39 -0300

    VERSAO2

--- app/src/main/java/br/edu/ifsp/arq/dmo_2020s1/conversortemperatura/ConversorTemperatura.java
+++ app/src/main/java/br/edu/ifsp/arq/dmo_2020s1/conversortemperatura/ConversorTemperatura.java
@@ -13,6 +13,7 @@ public class ConversorTemperatura extends AppCompatActivity implements View.OnCl
 
     private EditText entradaEditText;
     private Button fahrenheit2celsius;
+    private Button celsius2farenheit;
     private TextView saidaTextView;
 
     @Override
@@ -22,39 +23,57 @@ public class ConversorTemperatura extends AppCompatActivity implements View.OnCl
 
         entradaEditText = findViewById(R.id.edittext_valor);
         fahrenheit2celsius = findViewById(R.id.coverte_celsius);
+        celsius2farenheit = findViewById(R.id.coverte_fahrenheit);
         saidaTextView = findViewById(R.id.textview_valor_saida);
 
         fahrenheit2celsius.setOnClickListener(this);
+        celsius2farenheit.setOnClickListener(this);
     }
 
-        @Override
-        public void onClick (View v){
-            if (v == fahrenheit2celsius) {
-                converterParaCelsius();
-            }
+    @Override
+    public void onClick(View v) {
+        if (v == fahrenheit2celsius) {
+            converterParaCelsius();
         }
 
-        private double getEntrada () throws NumberFormatException {
-            double entrada;
-            try {
-                entrada = Double.valueOf(entradaEditText.getText().toString());
-            } catch (NumberFormatException e) {
-                entrada = 0;
-                throw e;
-            }
-
-            return entrada;
-        }
-
-        private void converterParaCelsius () {
-            double valor;
-            try {
-                valor = getEntrada();
-            } catch (NumberFormatException e) {
-                Toast.makeText(this, "Valor Digitado e invalido", Toast.LENGTH_SHORT).show();
-                valor = 0;
-            }
-
-            saidaTextView.setText(String.format("%.2f °C", (valor-32)/1.8));
+        if (v == celsius2farenheit) {
+            coverterParaFarenheit();
         }
     }
+
+    private double getEntrada() throws NumberFormatException {
+        double entrada;
+        try {
+            entrada = Double.valueOf(entradaEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            entrada = 0;
+            throw e;
+        }
+
+        return entrada;
+    }
+
+    private void converterParaCelsius() {
+        double valor;
+        try {
+            valor = getEntrada();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Valor Digitado e invalido", Toast.LENGTH_SHORT).show();
+            valor = 0;
+        }
+
+        saidaTextView.setText(String.format("%.2f °C", (valor - 32) / 1.8));
+    }
+
+    private void coverterParaFarenheit() {
+        double valor;
+        try {
+            valor = getEntrada();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Valor Digitado e invalido", Toast.LENGTH_SHORT).show();
+            valor = 0;
+        }
+
+        saidaTextView.setText(String.format("%.2f °F", 1.8 * valor + 32));
+    }
+}
